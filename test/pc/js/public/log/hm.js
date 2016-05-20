53e3e20695f5b14d375333e7787f2120c88262bb
yiwan_tang@163.com <yiwan_tang@163.com>
2016-05-20 11:37:27 +0800

    rm test/pc test/wap

--- test/pc/js/public/log/hm.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "//hm.baidu.com/hm.js?c8553bf999297be4b7c3bf2ffec7c37f";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
